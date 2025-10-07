a827cf5a3684fa480a1da30376bc63800eff5dbc
Liam555LLL <lpgillikin@students.wcpss.net>
2025-10-07 14:59:35 -0400

    this game tuff

--- game.py
+++ game.py
@@ -1,1 +1,24 @@
-print('Hello world')
+import random
+
+
+def create_number_guesser():
+    
+    print("guess a number between 1 and 100. I'll tell you if you get my number")
+    
+    rand_number = random.randint(1, 100)
+    guess = None
+    
+    while rand_number != guess:
+        s = input("Your guess: ")
+        guess = int(s)
+        if rand+number > guess:
+            print("Higher")
+        elif rand_number < guess:
+            print("Lower")
+        else:
+            print("You guessed it! The number is {}" .format(rand_number))
+            
+            
+    if __name__ == '__main__':
+    
+        create_number_guesser()
